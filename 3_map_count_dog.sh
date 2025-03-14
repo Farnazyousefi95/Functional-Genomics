57c33a14ab7f57b5be66bfd92a363206b165584a
Farnaz Yousefi <fzy0027@auburn.edu>
2025-03-13 21:25:54 -0500

    Update 3_map_count_dog.sh

--- 3_map_count_dog.sh
+++ 3_map_count_dog.sh
@@ -10,9 +10,9 @@
 ########## SLURM Directives
 #SBATCH --partition=bigmem
 #SBATCH --nodes=1
-#SBATCH --ntasks=6
-#SBATCH --mem=120G
-#SBATCH --time=12:00:00
+#SBATCH --ntasks=20
+#SBATCH --mem=200G
+#SBATCH --time=10:00:00
 #SBATCH --output=map_count_%j.o
 
 ########## Load Modules
@@ -25,36 +25,34 @@ module load samtools
 module load bcftools
 module load gffread
 
-########## Define Variables
-MyID=aubclsd0338              ## Replace with your ASC ID, e.g., aubtss
-WD=/scratch/${MyID}/DogRNAseq   ## Main project folder
-CD=${WD}/CleanData              ## Trimmed FASTQ files folder
-REFD=${WD}/DogRefGenome         ## Reference genome folder
-MAPD=${WD}/Map_HiSat2           ## Mapping output folder
-COUNTSD=${WD}/Counts_StringTie  ## StringTie counts folder
-RESULTSD=/home/${MyID}/DogRNAseq/Counts_H_S  ## Results in home directory
-REF=GCF_000002285.5             ## Tasha the Boxer genome accession
+# Define Variables
+MyID=aubclsd0338
+WD=/scratch/${MyID}/DogRNAseq
+CD=${WD}/CleanData
+REFD=${WD}/DogRefGenome
+MAPD=${WD}/Map_HiSat2
+COUNTSD=${WD}/Counts_StringTie
+RESULTSD=/home/${MyID}/DogRNAseq/Counts_H_S
+REF=GCF_000002285.5
 
-########## Create Directories
+# Create Directories
 mkdir -p $REFD
 mkdir -p $MAPD
 mkdir -p $COUNTSD
 mkdir -p $RESULTSD
 
-########## Prepare Reference Genome
-###It might have a problem in this step due to the file name( remember)
+# Copy Reference Files to Working Directory
 cd $REFD
-cp /home/${MyID}/class_shared/Dog_Tasha_GCF_000002285.5/data/${REF}.fna .
-cp /home/${MyID}/class_shared/Dog_Tasha_GCF_000002285.5/data/${REF}.gff .
+cp /scratch/${MyID}/final_project/ncbi_dataset/data/${REF}/GCF_000002285.5_Dog10K_Boxer_Tasha_genomic.fna .
+cp /scratch/${MyID}/final_project/ncbi_dataset/data/${REF}/genomic.gff .
 
-
-########## Convert Annotation and Extract Splice Sites/Exons
-gffread ${REF}.gff -T -o ${REF}.gtf
+# Convert Annotation to GTF and Extract Splice Sites/Exons
+gffread genomic.gff -T -o ${REF}.gtf
 hisat2_extract_splice_sites.py ${REF}.gtf > ${REF}.ss
 hisat2_extract_exons.py ${REF}.gtf > ${REF}.exon
 
-########## Build HiSat2 Index
-hisat2-build --ss ${REF}.ss --exon ${REF}.exon ${REF}.fna Dog_Tasha_index
+# Build HiSat2 Index
+hisat2-build --ss ${REF}.ss --exon ${REF}.exon GCF_000002285.5_Dog10K_Boxer_Tasha_genomic.fna Dog_Tasha_index
 
 ########## Map Trimmed Reads, Convert, Sort, and Count
 cd ${CD}
@@ -64,19 +62,19 @@ mv ${CD}/list .
 while read i;
 do
     ## Map with HiSat2
-    hisat2 -p 6 --dta --phred33 \
-    -x "${REFD}"/Dog_Tasha_index \
-    -1 "${CD}"/"$i"_1_paired.fastq -2 "${CD}"/"$i"_2_paired.fastq \
-    -S "$i".sam
+    hisat2 -p 20 --dta --phred33 \
+    -x "${REFD}/Dog_Tasha_index" \
+    -1 "${CD}/${i}_1_paired.fastq" -2 "${CD}/${i}_2_paired.fastq" \
+    -S "${i}.sam"
     
     ## Convert SAM to BAM and Sort with Samtools
-    samtools view -@ 6 -bS "$i".sam > "$i".bam
-    samtools sort -@ 6 "$i".bam -o "$i"_sorted.bam
-    samtools flagstat "$i"_sorted.bam > "$i"_Stats.txt
+    samtools view -@ 20 -bS "${i}.sam" > "${i}.bam"
+    samtools sort -@ 20 "${i}.bam" -o "${i}_sorted.bam"
+    samtools flagstat "${i}_sorted.bam" > "${i}_Stats.txt"
     
     ## Count with StringTie
-    mkdir "${COUNTSD}"/"$i"
-    stringtie -p 6 -e -B -G "${REFD}"/"${REF}".gtf -o "${COUNTSD}"/"$i"/"$i".gtf -l "$i" "${MAPD}"/"$i"_sorted.bam
+    mkdir -p "${COUNTSD}/${i}"
+    stringtie -p 20 -e -B -G "${REFD}/${REF}.gtf" -o "${COUNTSD}/${i}/${i}.gtf" -l "${i}" "${MAPD}/${i}_sorted.bam"
 done < list
 
 ########## Create Count Matrices
